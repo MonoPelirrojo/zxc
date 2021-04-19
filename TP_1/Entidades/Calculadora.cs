b80447078301ba9ab5f67fb7cdd2ed33bf047c77
LeanndroAG <leanndro.ps4@gmail.com>
2021-04-19 06:10:40 -0300

    TP_1

---
+++ TP_1/Entidades/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        #region Metodos
+        /// <summary>
+        /// Metodo privado de validacion.
+        /// <param name="operador">Operador a validar.</param>
+        /// <returns>Retorno operador validado en string.</returns>
+        private static string ValidarOperador(char operador)
+        {
+            switch (operador)
+            {
+                case '+':
+                    break;
+                case '-':
+                    break;
+                case '/':
+                    break;
+                case '*':
+                    break;
+                default:
+                    operador = '+';
+                    break;
+            }
+            return operador.ToString(); 
+        }
+        /// <summary>
+        /// Metodo estatico de operaciones.
+        /// </summary>
+        /// <param name="num1">Primero numero para operación</param>
+        /// <param name="num2">Segundo numero para operación</param>
+        /// <param name="operador">Tipo de operador</param>
+        /// <returns>Retorno resultado de la operacion</returns>
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado = 0;
+            switch (ValidarOperador(Convert.ToChar(operador)))
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+            }
+            return resultado;
+        }
+        #endregion
+    }
+
+}
---
+++ TP_1/Entidades/Numero.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        #region Atributos
+        private double numero;
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Propiedad solo escritura con validacion.
+        /// </summary>
+        private string SetNumero
+        {
+            set
+            {
+                this.numero = ValidarNumero(value);
+            }
+        }
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor que recibe un string
+        /// Constructor por defecto.
+        /// </summary>
+        /// <param name="strNumero">Recibo un numero para inicializar el objeto.</param>
+        public Numero(string strNumero)
+        {
+            this.SetNumero = strNumero;
+        }
+        /// <summary>
+        /// Constructor que recibe un double
+        /// </summary>
+        /// <param name="dbNumero">Recibo un numero para inicializar el objeto.</param>
+        public Numero(double dbNumero) : this(dbNumero.ToString())
+        {
+            
+        }
+        /// <summary>
+        /// Constructor sin parametro inicializa en cero
+        /// </summary>
+        public Numero() : this(0)
+        {
+
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Metodo para validar numero
+        /// </summary>
+        /// <param name="strNumero">Numero a validar.</param>
+        /// <returns>Retorno numero validado.</returns>
+        private static double ValidarNumero(string strNumero)
+        {
+            double.TryParse(strNumero, out double dbNumero);
+            return dbNumero;
+        }
+        /// <summary>
+        /// Metodo para convertir de binario a decimal.
+        /// </summary>
+        /// <param name="strResultado">Numero a convertir.</param>
+        /// <returns>Retorno el numero convertido en decimal.</returns>
+        public static string BinarioADecimal(string strResultado)
+        {
+            Numero dbNumero = new Numero(strResultado);
+            char[] arrResultado = strResultado.ToCharArray();
+            Array.Reverse(arrResultado);
+            double suma = 0;
+            if (EsBinario(strResultado))
+            {
+                for (int i = 0; i < arrResultado.Length; i++)
+                {
+                    if (arrResultado[i] == '1')
+                        suma += (int)Math.Pow(2, i);
+                }
+                strResultado = suma.ToString();
+            }
+            else if (dbNumero.numero == 0)
+                strResultado = "0";
+            else
+                strResultado = "Valor inválido";
+            return strResultado;
+        }
+
+        /// <summary>
+        /// Metodo para convertir de decimal a binario
+        /// </summary>
+        /// <param name="strResultado">Numero a convertir.</param>
+        /// <returns>Retorno el numero convertido en binario.</returns>
+        public static string DecimalABinario(string strResultado)
+        {
+            Numero auxiliar = new Numero(strResultado);
+            double dbNumero = Math.Floor(auxiliar.numero);
+            strResultado = "";
+            if (dbNumero > 0)
+            {
+                while (dbNumero > 0)
+                {
+                    if (dbNumero % 2 == 0)
+                        strResultado = "0" + strResultado;
+                    else
+                        strResultado = "1" + strResultado;
+                    dbNumero = (int)(dbNumero / 2);
+                }
+            }
+            else if (dbNumero == 0)
+                strResultado = "0";
+            else
+                strResultado = "Valor invalido";
+            return strResultado;
+        }
+        /// <summary>
+        /// Metodo para convertir de decimal a binario
+        /// </summary>
+        /// <param name="strResultado">Numero a convertir.</param>
+        /// <returns>Retorno el numero convertido en binario</returns>
+        public static string DecimalABinario(double dbResultado)
+        {
+            return DecimalABinario(dbResultado.ToString());
+        }
+
+        /// <summary>
+        /// Metodo para validar binario
+        /// </summary>
+        /// <param name="binario">cadena a chequear.</param>
+        /// <returns>Retorno true o false.</returns>
+        private static bool EsBinario(string binario)
+          {
+              bool retorno = true;
+              foreach (char item in binario)
+              {
+                  if (item != '0' && item != '1')
+                  {
+                      retorno = false;
+                      break;
+                  }
+              }
+              return retorno;
+         }
+
+        /*private static bool EsBinario(string binario)
+        {
+            bool retorno = true;
+            for (int i = 0; i < binario.Length; i++)
+            {
+                if (binario[i] != '0' && binario[i] != '1')
+                {
+                    retorno = false;
+                    break;
+                }
+            }
+
+            return retorno;
+        }*/
+        #endregion
+
+        #region Operaciones
+        /// <summary>
+        /// Operación suma
+        /// </summary>
+        /// <param name="numero1">Operador para la suma.</param>
+        /// <param name="numero2">Operador para la suma.</param>
+        /// <returns>Retorno resultado de la suma.</returns>
+        public static double operator +(Numero numero1, Numero numero2)
+        {
+            double resultado = numero1.numero + numero2.numero;
+            return resultado;
+        }
+        /// <summary>
+        /// Operacion resta
+        /// </summary>
+        /// <param name="numero1">Operador para la división.</param>
+        /// <param name="numero2">Operador para la división.</param>
+        /// <returns>Retorno resultado de la </returns>        
+        public static double operator -(Numero numero1, Numero numero2)
+        {
+            double resultado = numero1.numero - numero2.numero;
+            return resultado;
+        }
+        /// <summary>
+        /// Operacion division
+        /// </summary>
+        /// <param name="numero1">Operador para la división.</param>
+        /// <param name="numero2">Operador para la división.</param>
+        /// <returns>Retorno resultado de la división.</returns>
+        public static double operator /(Numero numero1, Numero numero2)
+        {
+            double resultado;
+            if (numero2.numero == 0)
+                resultado = double.MinValue;
+            else
+                resultado = numero1.numero / numero2.numero;
+            return resultado;
+        }
+        /// <summary>
+        /// Operacion multiplicacion
+        /// </summary>
+        /// <param name="numero1">Operador para la multiplicación.</param>
+        /// <param name="numero2">Operador para la multiplicación.</param>
+        /// <returns>Retorno resultado de la multiplicación.</returns>
+        public static double operator *(Numero numero1, Numero numero2)
+        {
+            double resultado = numero1.numero * numero2.numero;
+            return resultado;
+        }
+        #endregion
+    }
+}
